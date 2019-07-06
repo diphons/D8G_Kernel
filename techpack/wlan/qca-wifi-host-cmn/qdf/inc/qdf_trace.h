6364da9859a60a6af5e6bea436c27fa2d90598f8
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-07 02:12:24 +0900

    qcacld-3.0: disable bunch of more debug configs
    These hurt performance
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Change-Id: Ifccb53948523a70b9b98d5531a2267e1f9731547

--- techpack/wlan/qca-wifi-host-cmn/qdf/inc/qdf_trace.h
+++ techpack/wlan/qca-wifi-host-cmn/qdf/inc/qdf_trace.h
@@ -1301,8 +1301,10 @@ QDF_STATUS qdf_print_set_category_verbose(unsigned int idx,
  *
  * Return : Verbose enabled(true) or disabled(false) or invalid input (false)
  */
-bool qdf_print_is_category_enabled(unsigned int idx,
-				   QDF_MODULE_ID category);
+static inline bool qdf_print_is_category_enabled(unsigned int idx,
+				   QDF_MODULE_ID category) {
+	return false;
+}
 
 /**
  * qdf_print_is_verbose_enabled() - Get verbose information of a category for
@@ -1314,9 +1316,11 @@ bool qdf_print_is_category_enabled(unsigned int idx,
  *
  * Return : Verbose enabled(true) or disabled(false) or invalid input (false)
  */
-bool qdf_print_is_verbose_enabled(unsigned int idx,
+static inline bool qdf_print_is_verbose_enabled(unsigned int idx,
 				  QDF_MODULE_ID category,
-				  QDF_TRACE_LEVEL verbose);
+				  QDF_TRACE_LEVEL verbose) {
+	return false;
+}
 
 /**
  * qdf_print_clean_node_flag() - Clean up node flag for print control object
--- techpack/wlan/qca-wifi-host-cmn/qdf/linux/src/qdf_trace.c
+++ techpack/wlan/qca-wifi-host-cmn/qdf/linux/src/qdf_trace.c
@@ -3659,6 +3659,7 @@ QDF_STATUS qdf_print_set_category_verbose(unsigned int idx,
 }
 qdf_export_symbol(qdf_print_set_category_verbose);
 
+#if 0
 bool qdf_print_is_category_enabled(unsigned int idx, QDF_MODULE_ID category)
 {
 	QDF_TRACE_LEVEL verbose_mask;
@@ -3730,6 +3731,7 @@ bool qdf_print_is_verbose_enabled(unsigned int idx, QDF_MODULE_ID category,
 	return verbose_enabled;
 }
 qdf_export_symbol(qdf_print_is_verbose_enabled);
+#endif
 
 #ifdef DBG_LVL_MAC_FILTERING
 
