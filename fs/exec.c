92249f75c13693a4b364fce5a1bde8d8fcb4135e
diphons <diphons@gmail.com>
2023-05-09 22:58:25 +0700

    Import KernelSU from https://github.com/tiann/KernelSU/tree/main/kernel Also inline with https://github.com/diphons/D8G_Kernel_oxygen

--- fs/exec.c
+++ fs/exec.c
@@ -1683,6 +1683,10 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+#endif
 /*
  * sys_execve() executes a new program.
  */
@@ -1698,6 +1702,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 	bool is_su;
 
+#ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
