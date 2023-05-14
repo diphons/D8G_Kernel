3067fadfaeee702417964da678065987ce77da2a
diphons <diphons@gmail.com>
2023-05-14 15:47:56 +0700

    sched:fair: Switch to scaling linear

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -106,7 +106,7 @@ unsigned int sysctl_sched_use_walt_task_util = 1;
  * SCHED_TUNABLESCALING_LINEAR - scaled linear, *ncpus
  */
 enum sched_tunable_scaling sysctl_sched_tunable_scaling
-	= SCHED_TUNABLESCALING_LOG;
+	= SCHED_TUNABLESCALING_LINEAR;
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
