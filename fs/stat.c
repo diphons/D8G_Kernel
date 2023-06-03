d359bcd6e992aebbd2ab35a7270fae6ceffb449b
diphons <diphons@gmail.com>
2023-06-03 10:20:29 +0700

    Fixup

--- fs/stat.c
+++ fs/stat.c
@@ -99,7 +99,7 @@ int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 	unsigned int lookup_flags = 0;
 
 #ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, flags);
+	ksu_handle_stat(&dfd, &filename, &flag);
 #endif
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
